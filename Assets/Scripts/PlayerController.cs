3b45e419f7fa5a4da6518fa342ea700b0d8a1576
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-09-22 16:09:35 +0900

    Until 11.6.7 (p.466)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,26 +14,66 @@ public class PlayerController : MonoBehaviour {
    private AudioSource playerAudio; // 사용할 오디오 소스 컴포넌트
 
    private void Start() {
-       // 초기화
+        // 초기화
+        playerRigidbody = GetComponent<Regidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
    }
 
    private void Update() {
-       // 사용자 입력을 감지하고 점프하는 처리
+        // 사용자 입력을 감지하고 점프하는 처리
+        if (isDead) //사망시 처리를 더 이상 진행하지 않고 종료
+            return;
+        //마우스 왼쪽 버튼을 눌렀으며 && 최개 점프횟수(2)에 도달하지 않았다면
+        if (Input.GetMouseButtonDown(0) && jumpCount < 2)
+        {
+            //점프횟수 증가
+            jumpCount++;
+            //점프 직전 속도를 순간적으로 제로(0,0)로 변경
+            playerRigidbody.velocity = Vector2.zero;
+            //리지드바디에 위쪽으로 힘주기
+            playerRigidbody.AddForce(new Vector2(0, jumpForce));
+            //오디소 소스 재생
+            playerAudio.Play();
+        }
+        else if (Input.GetMouseButtonUp(0) && playerRigidbody.velocity.y > 0)   //마우스 왼쪽 버튼에서 손을 떼는 순간 && 속도의 y값이 양수라면(위로 상승 중) 현재 속도를 절반으로 변경
+            playerRigidbody.velocity *= 0.5f;
+        animator.SetBool("Grounded", isGrounded);
    }
 
    private void Die() {
-       // 사망 처리
+        // 사망 처리
+        animator.SetTrigger("Die");
+
+        //오디오 소스에 할당된 오디오 클립을 deathClip으로 변경
+        playerAudio.clip = deathClip;
+        //사망 효과음 재생
+        playerAudio.Play();
+
+        //속도를 제로(0,0)으로 변경
+        playerRigidbody.velocity = vector.zero;
+        //사망상태를 true로 변경
+        isDead = true;
+
    }
 
    private void OnTriggerEnter2D(Collider2D other) {
-       // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+        // 트리거 콜라이더를 가진 장애물과의 충돌을 감지
+        if (other.tag == "Dead" && !isDead)
+            Die();
    }
 
    private void OnCollisionEnter2D(Collision2D collision) {
        // 바닥에 닿았음을 감지하는 처리
+       if(collision.contacts[0].normal.y > 0.7f)
+        {
+            isGrounded = true;
+            jumpCount = 0;
+        }
    }
 
    private void OnCollisionExit2D(Collision2D collision) {
-       // 바닥에서 벗어났음을 감지하는 처리
+        // 바닥에서 벗어났음을 감지하는 처리
+        isGrounded = false;
    }
 }
