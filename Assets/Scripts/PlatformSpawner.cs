acc6d09c04ba22dc0a3ac7516faeb385d6ed7d88
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-05 09:19:33 +0900

    Update PlatformSpawner.cs

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -3,7 +3,7 @@
 // 발판을 생성하고 주기적으로 재배치하는 스크립트
 public class PlatformSpawner : MonoBehaviour {
     public GameObject platformPrefab; // 생성할 발판의 원본 프리팹
-    public int count = 10; // 생성할 발판의 개수
+    public int count = -1; // 생성할 발판의 개수
 
     public float timeBetSpawnMin = 1.25f; // 다음 배치까지의 시간 간격 최솟값
     public float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
