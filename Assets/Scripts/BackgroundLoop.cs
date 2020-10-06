7621a96710cb6cf23a7b1e67643b143820337120
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-06 18:49:43 +0900

    Update BackgroundLoop.cs

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -12,15 +12,15 @@ public class BackgroundLoop : MonoBehaviour {
     }
 
     private void Update() {
-        // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
-        if (width <= -transform.position.x)
+        // 현재 위치가 원점에서 오른쪽으로 width 이상 이동했을때 위치를 리셋
+        if (transform.position.x >= width)
             Reposition();
     }
 
     // 위치를 리셋하는 메서드
     private void Reposition() {
-        //현재 위치에서 오른쪽으로 가로 길이 *2 만큼 이동
-        Vector2 offset = new Vector2(width * 2f, 0);
-        transform.position = (Vector2)transform.position - offset;
+        //현재 위치에서 왼쪽으로 가로 길이 *2 만큼 이동
+        Vector2 offset = new Vector2(width * -2f, 0);
+        transform.position = (Vector2)transform.position + offset;
     }
 }
