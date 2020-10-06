1b58078f0323ed65dea51db1440f839c5bb5669e
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-06 18:23:15 +0900

    update
    왼쪽에서 새로운 발판 나오게끔 변경.

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -13,7 +13,7 @@ public class BackgroundLoop : MonoBehaviour {
 
     private void Update() {
         // 현재 위치가 원점에서 왼쪽으로 width 이상 이동했을때 위치를 리셋
-        if (transform.position.x <= -width)
+        if (width <= -transform.position.x)
             Reposition();
     }
 
@@ -21,6 +21,6 @@ public class BackgroundLoop : MonoBehaviour {
     private void Reposition() {
         //현재 위치에서 오른쪽으로 가로 길이 *2 만큼 이동
         Vector2 offset = new Vector2(width * 2f, 0);
-        transform.position = (Vector2)transform.position + offset;
+        transform.position = (Vector2)transform.position - offset;
     }
 }
