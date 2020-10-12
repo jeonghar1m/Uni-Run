35265e021c2978b885fb7caf89473fe13931bd39
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-12 09:57:13 +0900

    Update PlatformSpawner.cs

--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -5,12 +5,12 @@ public class PlatformSpawner : MonoBehaviour {
     public GameObject platformPrefab; // 생성할 발판의 원본 프리팹
     private int count = 10; // 생성할 발판의 개수
 
-    public float timeBetSpawnMin = 1.25f; // 다음 배치까지의 시간 간격 최솟값
-    public float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
+    private float timeBetSpawnMin = 1.25f; // 다음 배치까지의 시간 간격 최솟값
+    private float timeBetSpawnMax = 2.25f; // 다음 배치까지의 시간 간격 최댓값
     private float timeBetSpawn; // 다음 배치까지의 시간 간격
 
-    public float yMin = -3.5f; // 배치할 위치의 최소 y값
-    public float yMax = 1.5f; // 배치할 위치의 최대 y값
+    private float yMin = -3.5f; // 배치할 위치의 최소 y값
+    private float yMax = 1.5f; // 배치할 위치의 최대 y값
     private float xPos = -20f; // 배치할 위치의 x 값
 
     private GameObject[] platforms; // 미리 생성한 발판들
