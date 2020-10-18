df50152153e1d9b8bebb5a8f4c60064a7089ede4
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-18 21:01:58 +0900

    update

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,15 +4,17 @@
 public class Platform : MonoBehaviour {
     public GameObject[] obstacles; // 장애물 오브젝트들
     private bool stepped = false; // 플레이어 캐릭터가 밟았었는가
+    private Vector2 trapTarget;  //가시 이동 목표 지점
 
     // 컴포넌트가 활성화될때 마다 매번 실행되는 메서드
-    private void OnEnable() {
+    private void OnEnable()
+    {
         // 발판을 리셋하는 처리
         //밟힘 상태를 리셋
         stepped = false;
 
         //장애물 수 만큼 루프
-        for(int i=0;i<obstacles.Length;i++)
+        for (int i = 0; i < obstacles.Length; i++)
         {
             //현재 순번의 장애물을 1/3 확률로 활성화
             if (Random.Range(0, 3) == 0)
@@ -21,6 +23,26 @@ public class Platform : MonoBehaviour {
                 obstacles[i].SetActive(false);
         }
     }
+    //private void Update()
+    //{
+    //    int enableTrapNumber = -1;
+    //    OnEnable(ref enableTrapNumber);
+    //    if (enableTrapNumber == 0)
+    //    {
+    //        trapTarget = new Vector2(-2, 1.8f);
+    //        obstacles[enableTrapNumber].transform.position += Vector2.MoveTowards(transform.position, trapTarget, 0.1f);
+    //    }
+    //    else if (enableTrapNumber == 1)
+    //    {
+    //        trapTarget = new Vector2(0, 1.8f);
+    //        obstacles[enableTrapNumber].transform.position = Vector2.MoveTowards(transform.position, trapTarget, 0.1f);
+    //    }
+    //    else if (enableTrapNumber == 2)
+    //    {
+    //        trapTarget = new Vector2(2, 1.8f);
+    //        obstacles[enableTrapNumber].transform.position = Vector2.MoveTowards(transform.position, trapTarget, 0.1f);
+    //    }
+    //}
 
     void OnCollisionEnter2D(Collision2D collision) {
         // 플레이어 캐릭터가 자신을 밟았을때 점수를 추가하는 처리
