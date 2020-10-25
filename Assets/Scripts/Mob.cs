3071da3a1c5d9b3a9cd99a6eb3c6b14b9a4763a0
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-25 21:53:31 +0900

    add mob

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,49 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Mob : MonoBehaviour
+{
+    public GameObject mob;
+    private bool isMob = false;
+    private float width;
+    private float speed = 2.0f;
+    private void OnEnable()
+    {
+
+        if (Random.Range(0, 0) == 0)    //3분의 1 확률로 몹 활성화
+        {
+            mob.SetActive(true);
+            isMob = true;
+        }
+        else
+            mob.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isFirstMoving = true;
+        if (isMob && transform.position.x >= width && !GameManager.instance.isGameover)
+        {
+            if (isFirstMoving)  //맨 처음 왼쪽으로 이동
+            {
+                mob.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                if (mob.transform.localPosition.x == -4.0)
+                    isFirstMoving = false;
+            }
+            else if(!isFirstMoving) //왼쪽으로 최대한 이동 후
+            {
+                if (mob.transform.localPosition.x <= -4.0)
+                {
+                    mob.transform.Rotate(0, 180, 0);
+                    mob.transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
+                else if (mob.transform.localPosition.x >= 4.0)
+                {
+                    mob.transform.Rotate(0, 180, 0);
+                    mob.transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+            }
+        }
+    }
+}
