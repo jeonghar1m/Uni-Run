e7cc1d632ebb66a9deb8af9bdcc86bf80b4dd3df
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-10-29 02:04:12 +0900

    update

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -48,6 +48,12 @@ public class Platform : MonoBehaviour {
             else if (isTrap[i] && transform.position.x < width && !GameManager.instance.isGameover && obstacles[i].transform.localPosition.y >= 0.8) //i번째 가시가 활성화되고 발판이 플레이어에게서 멀어졌으며, 게임 오버상태가 아니며, 가시의 local y좌표가 0.8 이상일 때
                 obstacles[i].transform.Translate(Vector3.down * speed * Time.deltaTime);
         }
+
+        //발판을 위아래로 왔다갔다 하게 해주는 코드
+        if(transform.position.y >= 5.0)
+            this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        else if(transform.position.y <= -5.0)
+            this.transform.Translate(Vector3.down * speed * Time.deltaTime);
     }
 
     void OnCollisionEnter2D(Collision2D collision) {
