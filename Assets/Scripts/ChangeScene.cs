6f2b9170c35cf8d019e8312cc1f238f6ef1fb4bf
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-04 02:31:57 +0900

    update

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //LoadScene을 사용하기 위함.
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeGameScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
