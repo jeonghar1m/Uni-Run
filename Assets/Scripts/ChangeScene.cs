bbd391c767014dfb53d278c3c5d4545603de73e3
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-05 23:23:36 +0900

    update

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,6 +5,13 @@ using UnityEngine.SceneManagement;  //LoadScene을 사용하기 위함.
 
 public class ChangeScene : MonoBehaviour
 {
+    void Update()
+    {
+        // 게임을 재시작할 수 있게 하는 처리
+        if (Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene("Main");
+
+    }
     public void ChangeGameScene()
     {
         SceneManager.LoadScene("Main");
