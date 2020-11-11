66121832795c1532cdf66907e7764bc653abb350
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-12 02:21:37 +0900

    Update ScrollingObject.cs

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -6,7 +6,7 @@ public class ScrollingObject : MonoBehaviour {
 
     private void Update() {
         // 게임 오브젝트를 오른쪽으로 일정 속도로 평행 이동하는 처리
-        speed = GameManager.currentLevel * 10;
+        speed = 5 * (GameManager.currentLevel + 1);
         if(!GameManager.instance.isGameover)
             transform.Translate(Vector3.right * speed * Time.deltaTime);        //초당 speed의 속도로 왼쪽으로 평행 이동
     }
