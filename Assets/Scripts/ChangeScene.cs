42c4647d5581997ed89b762347f2c4b450134436
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-12 00:34:58 +0900

    new chapter

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,17 +5,11 @@ using UnityEngine.SceneManagement;  //LoadScene을 사용하기 위함.
 
 public class ChangeScene : MonoBehaviour
 {
-    void Update()
-    {
-        // 다음 스테이지로 이동할 때
-        if (Input.GetKeyDown(KeyCode.Return))
-            SceneManager.LoadScene("Main");
-
-    }
     public void ChangeGameScene()
     {
         SceneManager.LoadScene("Main");
     }
+
     public void ChangeHelpScene()
     {
         SceneManager.LoadScene("Help");
---
+++ Assets/Scripts/GoToNextChapter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //LoadScene을 사용하기 위함.
+
+public class GoToNextChapter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+     // 다음 스테이지로 이동할 때
+     if (Input.GetKeyDown(KeyCode.Return))
+        SceneManager.LoadScene("Main2");
+    }
+}
