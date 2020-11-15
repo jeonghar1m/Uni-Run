409e53b0314fc2b5f5ede490049e4525a08c4dff
Harim Jeong <harim97714@mail.hongik.ac.kr>
2020-11-15 22:48:30 +0900

    Update ScrollingObject.cs

--- Assets/Scripts/ScrollingObject.cs
+++ Assets/Scripts/ScrollingObject.cs
@@ -2,7 +2,7 @@
 
 // 게임 오브젝트를 계속 왼쪽으로 움직이는 스크립트
 public class ScrollingObject : MonoBehaviour {
-    public int speed = 0; // 이동 속도
+    private int speed = 0; // 이동 속도
 
     private void Update() {
         // 게임 오브젝트를 오른쪽으로 일정 속도로 평행 이동하는 처리
